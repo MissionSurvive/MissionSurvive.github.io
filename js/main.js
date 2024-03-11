ba6fac3cfc99ebe682700e08909207c50cb4cf73
MissionSurvive <kacper.fraszkiewicz@gmail.com>
2024-03-11 08:58:30 +0100

    Lab 2

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ const PATH = '/serviceworker.js';
          console.log("No it didn't. This happened: ", err)
          });
         }
+
+window.addEventListener("load", (event) => {
+        const statusDisplay = document.getElementById("status");
+        statusDisplay.textContent = navigator.onLine ? "Online" : "OFFline";
+        });
+
+window.addEventListener("offline", (event) => {
+        const statusDisplay = document.getElementById("status");
+        statusDisplay.textContent = "OFFline";
+        });
+              
+window.addEventListener("online", (event) => {
+        const statusDisplay = document.getElementById("status");
+        statusDisplay.textContent = "Online";
+});
--- serviceworker.js
+++ serviceworker.js
@@ -15,7 +15,7 @@ self.addEventListener('install', function (evt) {
  })
  );
 });
-self.addEventListener('fetch', function (evt) {
+/*self.addEventListener('fetch', function (evt) {
  // console.log(event.request.url);
  evt.respondWith(
  // Firstly, send request..
@@ -24,4 +24,27 @@ self.addEventListener('fetch', function (evt) {
  return caches.match(evt.request);
  })
  );
-});
+});*/
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+    if (response) {
+    return response; // Cache hit
+    }
+    return fetch(event.request.clone())
+    .then(function (response) {
+    if (!isSuccessful(response)) {
+    return response;
+    }
+    caches.open(CACHE_NAME)
+    .then(function (cache) {
+    cache.put(event.request, response.clone());
+    });
+    return response;
+    }
+    );
+    })
+    );
+   });
