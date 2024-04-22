894952d0021cd8250395cfc8b3c7a59cb3388d9d
MissionSurvive <kacper.fraszkiewicz@gmail.com>
2024-04-22 09:23:24 +0200

    Speech recognition

--- script.js
+++ script.js
@@ -1,46 +1,101 @@
-function getUserMedia(constraints) {
-    // if Promise-based API is available, use it
-    if (navigator.mediaDevices) {
-      return navigator.mediaDevices.getUserMedia(constraints);
-    }
-      
-    // otherwise try falling back to old, possibly prefixed API...
-    var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia || navigator.msGetUserMedia;
-      
-    if (legacyApi) {
-      // ...and promisify it
-      return new Promise(function (resolve, reject) {
-        legacyApi.bind(navigator)(constraints, resolve, reject);
-      });
-    }
-  }
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+
+var recognition = new SpeechRecognition();
+if (SpeechGrammarList) {
+  var speechRecognitionList = new SpeechGrammarList();
+  var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+}
+recognition.continuous = false;
+recognition.lang = 'pl-PL';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var diagnostic = document.querySelector('.output');
+var bg = document.querySelector('html');
+var hints = document.querySelector('.hints');
+
+var colorHTML= '';
+colors.forEach(function(v, i, a){
+  console.log(v, i);
+  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+});
+hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
+
+document.body.onclick = function() {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
+
+recognition.onresult = function(event) {
   
-  function getStream (type) {
-    if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-      !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-      alert('User Media API not supported.');
-      return;
-    }
-  
-    var constraints = {};
-    constraints[type] = true;
-    
-    getUserMedia(constraints)
-      .then(function (stream) {
-        var mediaControl = document.querySelector(type);
-        
-        if ('srcObject' in mediaControl) {
-          mediaControl.srcObject = stream;
-        } else if (navigator.mozGetUserMedia) {
-          mediaControl.mozSrcObject = stream;
-        } else {
-          mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-        }
-        
-        mediaControl.play();
-      })
-      .catch(function (err) {
-        alert('Error: ' + err);
-      });
+  console.log(event.results[0][0].transcript);
+  var color = event.results[0][0].transcript;
+  switch(color) {
+    case "akwamaryna":
+      color = "aqua";
+      break;
+    case "lazur":
+      color = "azure";
+      break;
+      case "beżowy":
+        color = "beige";
+        break;
+        case "bisque":
+      color = "bisque";
+      break;
+      case "czarny":
+      color = "black";
+      break;
+      case "niebieski":
+      color = "blue";
+      break;
+      case "brązowy":
+      color = "brown";
+      break;
+      case "czekoladowy":
+      color = "chocolate";
+      break;
+      case "koralowy":
+      color = "coral";
+      break;
+      case "karmazynowy":
+      color = "crimson";
+      break;
+      case "cyjan":
+      color = "cyan";
+      break;
+      case "fuksja":
+      color = "fuchsia";
+      break;
+      case "widmo biały":
+      color = "ghostwhite";
+      break;
+      case "złoty":
+      color = "gold";
+      break;
+      case "nawłoć":
+      color = "goldenrod";
+      break;
   }
+  diagnostic.textContent = 'Result received: ' + color + '.';
+  bg.style.backgroundColor = color;
+  console.log('Confidence: ' + event.results[0][0].confidence);
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  diagnostic.textContent = "I didn't recognise that color.";
+}
+
+recognition.onerror = function(event) {
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
